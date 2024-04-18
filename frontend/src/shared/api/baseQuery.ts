1e5884360f8625581402d5ec1b55c0d330cd6e5c
nerpaFront <167439716+nerpaFront@users.noreply.github.com>
2024-04-19 08:48:13 +1000

    fix: port fix

--- frontend/src/shared/api/baseQuery.ts
+++ frontend/src/shared/api/baseQuery.ts
@@ -1,9 +1,8 @@
 import { fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 
-const baseUrl = "http://localhost:8009/api/v1/";
+const baseUrl = "http://localhost:8010/api/v1/";
 
 export const baseQuery = fetchBaseQuery({
-  mode: "no-cors",
   prepareHeaders: (headers) => {
     headers.set("Content-Type", "application/json");
     return headers;
